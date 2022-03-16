f135357f285a0be7c41b09f64e70a7c7d55662fc
CRgarcia15 <cg58708@gmial.com>
2022-03-15 20:10:28 -0400

    changed font size on navbar

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+
+for(let i = 0; i < weeklyExpensesQuestions.length; i++){
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+weeklyExpenses = weeklyExpenses + numberAnswer
+}
+
